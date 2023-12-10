581a40ef8ec35808dd73eee2e90d616bf57e1192
Flo-bgnn <florent.beguin38@gmail.com>
2023-12-10 19:32:31 +0100

    Update gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Your Name',
-    site_url: 'portfolio-sepia-nu-37.vercel.app',
-    feed_url: 'portfolio-sepia-nu-37.vercel.app/feed.xml'
+    site_url: 'portfolio-v1-dun-zeta.vercel.app',
+    feed_url: 'portfolio-v1-dun-zeta.vercel.app/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
