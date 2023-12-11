49404f789e91e62cbac89cd04ac7f6e0ade664a2
Flo-bgnn <florent.beguin38@gmail.com>
2023-12-11 21:36:40 +0100

    gh

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-import 'nextra-theme-blog/style.css';
-import Head from 'next/head';
-import '../styles/main.css';
+import 'nextra-theme-blog/style.css'
+import Head from 'next/head'
+
+import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
   return (
@@ -20,19 +21,9 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <Head>
-      
-        {/* Ajoutez le lien RSS ici pour qu'il apparaisse en premier */}
-        <link pages/index.mdx
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
 
