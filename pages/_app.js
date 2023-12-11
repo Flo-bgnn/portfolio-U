b11d301190f61c2a86c8e804e6a82ab39e189d05
Flo-bgnn <florent.beguin38@gmail.com>
2023-12-11 17:24:00 +0100

    ih

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,9 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
-import { useState } from 'react';
 
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
   return (
     <>
       <Head>
@@ -28,19 +21,9 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
-      <nav className={`menu ${menuOpen ? 'open' : ''}`}>
-        <button onClick={toggleMenu}>Toggle Menu</button>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          {/* Add more menu items as needed */}
-        </ul>
-      </nav>
       <Component {...pageProps} />
     </>
-  );
+  )
 }
+
+
