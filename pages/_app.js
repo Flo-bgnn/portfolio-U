ace285c000ad82eec7147cd7a02499b921e5cac9
Flo-bgnn <florent.beguin38@gmail.com>
2023-12-11 16:23:44 +0100

    g

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import 'nextra-theme-blog/style.css'
 import Head from 'next/head'
+import { useState } from 'react';
 
 import '../styles/main.css'
 
 export default function Nextra({ Component, pageProps }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <Head>
@@ -21,7 +28,19 @@ export default function Nextra({ Component, pageProps }) {
           crossOrigin="anonymous"
         />
       </Head>
+      <nav className={`menu ${menuOpen ? 'open' : ''}`}>
+        <button onClick={toggleMenu}>Toggle Menu</button>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          {/* Add more menu items as needed */}
+        </ul>
+      </nav>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
--- pages/posts/ceportfolio.md
+++ pages/posts/ceportfolio.md
@@ -8,3 +8,9 @@ type: post
 ---
 
 # Ce portfolio 
+
+## Structure du sîte
+
+### theme.config.js
+
+Ce code est un module JavaScript qui exporte un objet avec une propriété "footer". Le pied de page (footer) est défini comme un élément JSX (JavaScript XML) avec une balise <small>. À l'intérieur de cet élément, il y a un élément <time> qui affiche l'année actuelle obtenue à l'aide de la fonction new Date().getFullYear(). Mon nom est affiché avec l'année, suivi d'un lien "RSS". Le style du pied de page est défini en utilisant les propriétés CSS en ligne dans la balise <small>. De plus, le style spécifique au module est déclaré à l'aide de la syntaxe styled-jsx, avec une règle de style pour un lien (a) qui flotte à droite, et une règle de style conditionnelle basée sur la largeur de l'écran.
