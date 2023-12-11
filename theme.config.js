fbb822e685f50c18597a8d8da5102b3c1679b9ed
Flo-bgnn <florent.beguin38@gmail.com>
2023-12-12 00:41:22 +0100

    , jkl

--- theme.config.js
+++ theme.config.js
@@ -2,7 +2,7 @@ const YEAR = new Date().getFullYear()
 
 export default {
   footer: (
-    <small style={{ display: 'block', marginTop: '8rem', backgroundColor: 'red' }}>
+    <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Florent BEGUIN.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
